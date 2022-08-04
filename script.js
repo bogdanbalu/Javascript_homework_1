d007ea44012b06f2d89094cf9510f793996f8fc1
Bogdan Balu <bogdan_va2006@yahoo.com>
2022-08-04 19:02:50 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,60 @@
+console.log("Returnează un boolean dacă un număr este divizibil cu 10");
+const DEVID = 10;
+const ZERO = 0;
+
+const isDivisible = (number) => {
+    return ((number % DEVID) !== ZERO) ? false : true;
+}
+
+const result = isDivisible(100);
+console.log(result);
+
+console.log("--------------------------------------------------------------");
+
+console.log("Scrieți o funcție numită tellFortune");
+
+const numberOfChildren = 2;
+const partnerName = "Oana";
+const location = "Sibiu";
+const job = "Software Developer";
+
+const tellFortune = (numberOfChildren,partnerName, location, job ) => {
+    return `Vei fi un: ${job}, în: ${location}, căsătorit cu: ${partnerName}, și vei avea: ${numberOfChildren} copii`;
+}
+
+const resultTellFortune = tellFortune(numberOfChildren, partnerName, location, job);
+console.log(resultTellFortune);
+
+console.log("--------------------------------------------------------------");
+
+console.log("Folosind instrucțiunea JavaScript switch se va obține ziua săptămânii");
+
+const currentDayNumber = 1;
+
+const getCurrentDay = (currentDay) => {
+    switch(currentDay) {
+        case 1: {
+            return "Mondey"
+        }
+        case 2: {
+            return "Tuesday"
+        }
+        case 3: {
+            return "Wednesday"
+        }
+        case 4: {
+            return "Thursday"
+        }
+        case 5: {
+            return "Friday"
+        }
+        case 6: {
+            return "Saturday"
+        }
+        case 7: {
+            return "Sunday"
+        }  
+    }
+}
+ const currentDay = getCurrentDay(currentDayNumber);
+ console.log(`The current day is: ${currentDay}`);
